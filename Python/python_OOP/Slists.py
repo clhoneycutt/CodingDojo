8df63c87e2bdd2cfd86392a8394da5cb2fccd232
Chris Honeycutt <clhoneycutt@gmail.com>
2018-12-04 01:31:09 -0500

    Slists.py - completed Single Linked List assignment

--- Python/python_OOP/Slists.py
+++ Python/python_OOP/Slists.py
@@ -8,22 +8,35 @@ class SList:
         node = Node(value)
         self.head = node
     
-    def addNode(self, value):
-        node = Node(value)
+    def addNode(self, addValue):
+        node = Node(addValue)
         runner = self.head
         while(runner.next != None):
             runner = runner.next
         runner.next = node
 
-    def removeNode(self,value):
-        node = Node(value)
+    def removeNode(self,delValue):
         runner = self.head
-        while(runner.next != None):
-            if node.value == value:
-                runner = runner.next.next
-            
+        if runner is not None:
+            if runner.value == delValue:
+                self.head = runner.next
+                runner = None
+                return
         
+        while(runner.next != None):
+            if runner.value == delValue:
+                break
+            prev = runner
+            runner = runner.next
 
+        if runner == None:
+            print(delValue, " Not Found")
+            return
+
+        prev.next = runner.next
+
+        runner = None
+       
     def printAllValues(self):
         runner = self.head
         while(runner.next != None):
@@ -35,4 +48,13 @@ class SList:
 
 x = SList(1)
 x.addNode(5)
+x.addNode(23)
+x.addNode(12)
+x.addNode(2)
+x.addNode(125)
+x.printAllValues()
+print("\n\n","="*50,"\n\n")
+x.removeNode(1)
+x.removeNode(23)
+# x.removeNode(85)
 x.printAllValues()

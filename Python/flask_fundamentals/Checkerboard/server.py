90649ddd3e320ed7f35327e91c9fbf1965b13980
Chris Honeycutt <clhoneycutt@gmail.com>
2018-12-05 13:03:19 -0500

    started checkerboard assignment - using jinja

---
+++ Python/flask_fundamentals/Checkerboard/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/<x>/<y>')
+
+def index(x, y):
+    x = int(x)
+    y = int(y)
+    return render_template('index.html', x=x, y=y)
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
